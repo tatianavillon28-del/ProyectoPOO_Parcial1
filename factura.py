7f6c002dfae0a65b7fd0789c4f97e4d3ae4c898e
tatianavillon28-del <tatianavillon28@gmail.com>
2026-06-18 13:51:29 -0500

    Update factura.py

--- factura.py
+++ factura.py
@@ -1,7 +1,7 @@
 # Integrantes:
-# -Villón Montenegro Tatiana Jazmin
-# -Rivera Rivas  Irina Alexandra 
-# -Tomala Lino Karen Dayana 
+# - Villón Montenegro Tatiana Jazmin
+# - Rivera Rivas Irina Alexandra
+# - Tomala Lino Karen Dayana
 
 class Factura:
 
@@ -9,8 +9,31 @@ class Factura:
         self.numero = numero
         self.paciente = paciente
 
+    @property
+    def numero(self):
+        return self.__numero
+
+    @numero.setter
+    def numero(self, valor):
+        if valor <= 0:
+            raise ValueError("Número de factura inválido")
+        self.__numero = valor
+
+    @property
+    def paciente(self):
+        return self.__paciente
+
+    @paciente.setter
+    def paciente(self, valor):
+        if valor is None:
+            raise ValueError("Debe asignar un paciente")
+        self.__paciente = valor
+
     def generar_factura(self):
         return (
             f"Factura N° {self.numero}\n"
             f"Paciente: {self.paciente.nombre}"
         )
+
+    def __str__(self):
+        return self.generar_factura() 
